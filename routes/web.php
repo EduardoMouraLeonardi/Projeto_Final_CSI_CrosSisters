2df07d1f0eeb5ad715bd55fc5371d50081fe3ba4
Pedro <126927099+Papacyto@users.noreply.github.com>
2023-09-05 20:03:33 -0300

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// CATEGORIA 
+
+Route::get('/categoria', [CategoriaController::class,'index'])->name('categoria.index');
