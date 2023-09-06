1d87510b83bc4da27dae200e6f07d1e4f47a15f2
Pedro <126927099+Papacyto@users.noreply.github.com>
2023-09-05 21:06:57 -0300

    CategoriaController.php

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -9,7 +9,72 @@ class CategoriaController extends Controller
 {
     public function index()
     {
-        $categoria = Categoria::orderby('nome')->get();
-        dd($categoria);
+        $categoria = Categoria::get();
+        return view('categoria.categoria_index', ['categoria' => $categoria]);
     }
+
+    public function create()
+    {
+        return view('categoria.categoria_create');
+    }
+
+    public function store(Request $request)
+    {
+        //dd($request->all());
+
+        $messages = [
+            'nome.required' => 'O :attribute é obrigatório.',
+        ];
+
+        $validated = $request->validate([
+            'nome' => 'required|min:5',
+        ], $messages);
+
+        $categoria = new Categoria();
+        $categoria->nome = $request->nome;
+        $categoria->save();
+
+        return redirect()->route('categoria.index')->with('status', 'Categoria criada com sucesso!');
+    }
+
+    public function show(string $id)
+    {
+        $categoria = Categoria::find($id);
+        //dd($produto);
+        return view('categoria.categoria_show', ['categoria' => $categoria]);
+    }
+
+    public function edit(string $id)
+    {
+        $categoria = Categoria::find($id);
+        return view('categoria.categoria_edit', ['categoria' => $categoria]);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $messages = [
+            'nome.required' => 'O :attribute é obrigatório.',
+        ];
+
+        $validated = $request->validate([
+            'nome' => 'required|min:5',
+        ], $messages);
+
+        $categoria = Categoria::find($id);
+        $categoria->nome = $request->nome;
+        $categoria->save();
+
+        return redirect()->route('categoria.index')->with('status', 'Categoria alterada com sucesso!');
+    }
+
+    public function destroy(string $id)
+    {
+        $categoria = Categoria::find($id);
+        $categoria->delete();
+
+        return redirect()->route('categoria.index')->with('status', 'Categoria excluída com sucesso!');
+    }
+
+}
+
 }
