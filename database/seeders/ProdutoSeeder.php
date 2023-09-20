1c5ef5081f43b84c6afc654119c5bed2a166849c
Eduardo Moura Leonardi <eduardo.moura.leonardi@gmail.com>
2023-09-19 21:37:00 -0300

    Update ProdutoSeeder.php

--- database/seeders/ProdutoSeeder.php
+++ database/seeders/ProdutoSeeder.php
@@ -17,21 +17,21 @@ class ProdutoSeeder extends Seeder
             'nome' => 'Top_01',
             'preco' => 15.99,
             'quantidade' => 65,
-            'categoria-id'=>1
+            'categoria_id'=>1
         ]);
 
         DB::table('produtos')->insert([
             'nome' => 'Saia_01',
             'preco' => 19.99,
             'quantidade' => 30,
-            'categoria-id'=>1
+            'categoria_id'=>1
         ]);
 
         DB::table('produtos')->insert([
             'nome' => 'Legging_01',
             'preco' => 25.99,
             'quantidade' => 100,
-            'categoria-id'=>2
+            'categoria_id'=>2
         ]);
     }
 }
