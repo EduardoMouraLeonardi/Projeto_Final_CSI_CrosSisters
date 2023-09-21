6385da080e454dcec4070d9e072154e1aba15a13
Pedro <126927099+Papacyto@users.noreply.github.com>
2023-09-20 21:12:36 -0300

    AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,7 +23,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('is_admin', function (User $user, ) {
+        Gate::define('is_admin', function (User $user) {
             return $user->perfil === 'admin';
         });
     }
