36081aee870580a4a3d9dc00844d12cf6f7f7c7e
Pedro <126927099+Papacyto@users.noreply.github.com>
2023-09-26 21:06:51 -0300

    email_create.blade.php

--- resources/views/email/email_create.blade.php
+++ resources/views/email/email_create.blade.php
@@ -19,14 +19,17 @@
     
     @csrf
 
-  <label class="form-label" for="para">Para:</label><br>
-  <input class="form-control" type="email" id="para" name="para"><br>
+  <label class="form-label" for="nome">Nome:</label><br>
+  <input class="form-control" type="texte" id="nome" name="nome"><br>
+
+  <label class="form-label" for="email2">Email:</label><br>
+  <input class="form-control" type="email" id="email2" name="email2"><br>
 
   <label class="form-label" for="assunto">Assunto:</label><br>
   <input class="form-control" type="text" id="assunto" name="assunto"><br>
 
   <label class="form-label" for="conteudo">Conteúdo:</label><br>
-  <textarea class="form-control" type="text" id="conteudo" name="conteudo" rows="3"></textarea> <br>
+  <textarea class="form-control" type="text" id="conteudo" name="conteudo" rows="5"></textarea> <br>
 
   <input type="submit" value="Enviar">
 
