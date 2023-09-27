7a59f38a72fb1784b09671536add0c12b26ad895
Eduardo Moura Leonardi <eduardo.moura.leonardi@gmail.com>
2023-09-26 21:53:34 -0300

    Update pages.blade.php

--- resources/views/admin/pages.blade.php
+++ resources/views/admin/pages.blade.php
@@ -1,5 +1,24 @@
 @extends('adminlte::page')
 
 @section('content')
+<div class="container">
 
+<h2>Paginas<h2><br>
+
+<table>
+
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+
+</table>
+
+</div>
 @endsection
