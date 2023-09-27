0eb401b037059efcf36d87bfd764ab7ddbd4cf78
Pedro <126927099+Papacyto@users.noreply.github.com>
2023-09-26 21:06:29 -0300

    email_show.blade.php

--- resources/views/email/email_show.blade.php
+++ resources/views/email/email_show.blade.php
@@ -5,7 +5,8 @@
     
 <h1>Email</h1><br>
 
-<strong>Para: </strong> {{ $email->para }}<br>
+<strong>Nome: </strong> {{ $email->nome }}<br>
+<strong>Email: </strong> {{ $email->email2 }}<br>
 <strong>Assunto: </strong> {{ $email->assunto }}<br>
 <strong>Conteúdo: </strong> {{ $email->conteudo }}<br>
 <strong>Criação: </strong> {{ $email->created_at }}<br>
