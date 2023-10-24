2fb415c6e78c2585c5665c95c9ffeb9e26588eb0
Pedro <pedrohenrriquefreitas135@gmail.com>
2023-10-24 20:48:35 -0300

    Update Pedido.php

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -4,12 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use OwenIt\Auditing\Contracts\Auditable;
 
-class Pedido extends Model
+class Pedido extends Model implements Auditable
 {
     use HasFactory;
     use \OwenIt\Auditing\Auditable;
 
     protected $table = 'pedido';
+    protected $fillable = [
+        'filename',
+        'path'
+    ];
+
+    public function produtos(): HasMany
+    {
+        return $this->HasMany(Produto::class, 'id', 'produto_id');
+    }
 }
