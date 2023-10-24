17ee314c71ecdc871aec1d801a4c5b43c91594dd
Pedro <pedrohenrriquefreitas135@gmail.com>
2023-10-24 19:51:45 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -181,5 +181,5 @@
 		<li>&copy; Endereço loja Física: Avenida Getúlio Vargas, 3663 Canoas/RS</li> <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
 	</ul>
       </footer>
-
+      
 @endsection
