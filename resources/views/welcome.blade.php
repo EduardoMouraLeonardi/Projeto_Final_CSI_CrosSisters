d51e4c9f3d79de8612d20f38c4ef962c03c8d7f2
opsstaissa <taissamariana13@gmail.com>
2023-11-21 21:31:08 -0300

    adminLTE

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -129,7 +129,7 @@
 
 <div class="row featurette">
           <div class="col-md-7">
-            <h2 class="featurette-heading">Primeiro título de featurezinhas. <span class="text-muted">Supreendente, não?!</span></h2>
+            <h2 class="featurette-heading">Supreendente, não?! <span class="text-muted"> Olha esse conjunto!!</span></h2>
             <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
           </div>
           <div class="col-md-5">
@@ -141,7 +141,7 @@
 
         <div class="row featurette">
           <div class="col-md-7 order-md-2">
-            <h2 class="featurette-heading">Aêêê, moleque! <span class="text-muted">Tá legal ou não tá?</span></h2>
+            <h2 class="featurette-heading">Aêêê, mulherada! <span class="text-muted">Tá legal ou não tá?</span></h2>
             <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
           </div>
           <div class="col-md-5 order-md-1">
