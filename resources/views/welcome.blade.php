c90b7292ef64ef0a90a6eaf214f1ed5297d62d77
eusamarax <141786177+eusamarax@users.noreply.github.com>
2023-11-27 21:11:45 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -111,6 +111,7 @@
             <img class="rounded-circle" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7Vb1Ilcibte73KPY-xweVzaD34EK5WJY6AQ&usqp=CAU" alt="Generic placeholder image" width="140" height="140">
             <h2>Short</h2>
             <p>O Short de academia é uma das peças coringas para você montar o seu look. Sabemos o quanto é difícil combinar looks para academia e, por isso, temos peças de cores básicas para você criar várias combinações diferentes.</p>
+            <br>
             <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
           </div><!-- /.col-lg-4 -->
           <div class="col-lg-4">
